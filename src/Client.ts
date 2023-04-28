8a3ed9c76a93597eb6606dd19d203a9b581ee4c9
Oussama Benhamed <wisleur@gmail.com>
2023-04-28 22:54:32 +0200

    refactor: get rid of monkey-patching Dynamodb connection config

--- src/Client.ts
+++ src/Client.ts
@@ -7,25 +7,21 @@ import { Dyngoose } from 'dyngoose'
 export default class Client {
   private emitter: EmitterContract
 
-  constructor (private app: ApplicationContract, private config: DynamoDBConfig) {
+  constructor(private app: ApplicationContract, private config: DynamoDBConfig) {
     this.emitter = this.app.container.use('Adonis/Core/Event')
 
-    // @ts-ignore
-    Dyngoose.Config.default.setConnection = function (connection: typeof Dyngoose.Connection) {
-      this.__defaultConnection = connection
-    }
-
-    // @ts-ignore
-    Dyngoose.Config.default.setConnection(new Dyngoose.Connection.DynamoDBConnection(this.config))
+    Dyngoose.Config.default['__defaultConnection'] = new Dyngoose.Connection.DynamoDBConnection(
+      this.config
+    )
 
     this.emitStartEvent()
   }
 
-  public getClient (): DynamoDBContract {
+  public getClient(): DynamoDBContract {
     return Dyngoose
   }
 
-  private async emitStartEvent (): Promise<void> {
+  private async emitStartEvent(): Promise<void> {
     await this.emitter.emit('dynamodb:start', '')
   }
 }
