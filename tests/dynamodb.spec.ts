bdbdb90963e5af6c5c14e61117699eca35fc8c42
Oussama Benhamed <wisleur@gmail.com>
2023-04-29 04:12:08 +0200

    test: try to debug GH actions

--- tests/dynamodb.spec.ts
+++ tests/dynamodb.spec.ts
@@ -13,68 +13,86 @@ test.group('DynamoDB Client', (group) => {
   })
 
   test('client should create table and put items', async ({ expect }) => {
-    const app = await setup('test', dynamoDBConfig)
-    const DynamoDB = new Client(app, dynamoDBConfig).getClient()
-
-    @DynamoDB.$Table({
-      name: 'AdonisCardTableDynamoDBTest'
-    })
-    class Card extends DynamoDB.Table {
-      @DynamoDB.$PrimaryKey('id')
-      public static readonly primaryKey: Query.PrimaryKey<Card, number, void>
-
-      @DynamoDB.Attribute.Number()
-      public id: number
-
-      @DynamoDB.Attribute.String()
-      public title: string
-
-      @DynamoDB.Attribute.Date({ timeToLive: true })
-      public expiresAt: number | Date
-    }
+    console.log(0)
 
     try {
-      await Card.createTable()
-    } catch {
-      console.log('Table already created')
+      const app = await setup('test', dynamoDBConfig)
+      const DynamoDB = new Client(app, dynamoDBConfig).getClient()
+
+      console.log(1)
+
+      @DynamoDB.$Table({
+        name: 'AdonisCardTableDynamoDBTest'
+      })
+      class Card extends DynamoDB.Table {
+        @DynamoDB.$PrimaryKey('id')
+        public static readonly primaryKey: Query.PrimaryKey<Card, number>
+
+        @DynamoDB.Attribute.Number()
+        public id: number
+
+        @DynamoDB.Attribute.String()
+        public title: string
+
+        @DynamoDB.Attribute.Date({ timeToLive: true })
+        public expiresAt: number | Date
+      }
+
+      console.log(2)
+
+      try {
+        await Card.createTable()
+
+        console.log(3)
+      } catch {
+        console.log('Table already created')
+      }
+
+      const date = +new Date() + 60 * 60 * 1000
+
+      const card = new Card()
+      card.id = 15
+      card.title = 'Test'
+      card.expiresAt = date
+      await card.save()
+
+      console.log(4)
+
+      await Card.new({
+        id: 99065,
+        title: 'Foo',
+        expiresAt: date
+      }).save()
+
+      console.log(5)
+
+      const records = await Card.primaryKey.scan()
+      const results: Array<Record<string, unknown>> = []
+
+      for (const record of records) {
+        results.push(record.toJSON())
+      }
+
+      expect(results).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: 15,
+            title: 'Test',
+            expiresAt: date
+          }),
+          expect.objectContaining({
+            id: 99065,
+            title: 'Foo',
+            expiresAt: date
+          })
+        ])
+      )
+
+      await Card.deleteTable()
+    } catch (e) {
+      console.log(10)
+
+      throw e
     }
-
-    const date = +new Date() + 60 * 60 * 1000
-
-    const card = new Card()
-    card.id = 15
-    card.title = 'Test'
-    card.expiresAt = date
-    await card.save()
-
-    await Card.new({
-      id: 99065,
-      title: 'Foo',
-      expiresAt: date
-    }).save()
-
-    const records = await Card.primaryKey.scan()
-    const results: Array<Record<string, unknown>> = []
-
-    for (const record of records) {
-      results.push(record.toJSON())
-    }
-
-    expect(results).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: 15,
-          title: 'Test',
-          expiresAt: date
-        }),
-        expect.objectContaining({
-          id: 99065,
-          title: 'Foo',
-          expiresAt: date
-        })
-      ])
-    )
-
-    await Card.deleteTable()
   }).disableTimeout()
 })
